e1b65a7ad94a7144c2b8c8b6161bb5bf22084bba
legacyofmu <legacyofmu1@gmail.com>
2025-05-16 01:54:35 +0900

    Use dodecahedral dice and add some rule

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use rand::Rng;
 use std::io;
 
 fn main() {
-    println!("Welcome Dice Game!");
+    println!("Welcome to Dodecahedral Dice Game!");
     println!("Press Enter to start. Press <q> to exit.");
+
+    let mut point: Option<u8> = None;
 
     loop {
         let mut input = String::new();
@@ -14,7 +16,37 @@ fn main() {
             break;
         }
 
-        let dice = rand::thread_rng().gen_range(1..=6);
+        let dice = rand::thread_rng().gen_range(1..=12);
         println!("Dice result: {}", dice);
+
+        match point {
+            None => handle_first_roll(dice, &mut point),
+            Some(p) => handle_point_roll(dice, p, &mut point),
+        }
+    }
+}
+
+fn handle_first_roll(dice: u8, point: &mut Option<u8>) {
+    if dice == 7 || dice == 11 {
+        println!("Congratulation! You win on the first try.");
+    } else if dice == 2 || dice == 3 || dice == 12 {
+        println!("Too bad, you lost on the first try.");
+    } else {
+        println!("Point Setting: {}. Roll this number again and you win!", dice);
+        *point = Some(dice);
+    }
+}
+
+fn handle_point_roll(dice: u8, target: u8, point: &mut Option<u8>) {
+    match dice {
+        7 => {
+            println!("You got a 7. You've lost.");
+            *point = None;
+        }
+        _ if dice == target => {
+            println!("Point {} achieved! You win!", target);
+            *point = None;
+        }
+        _ => println!("Current target: {}. Roll again.", target),
     }
 }
